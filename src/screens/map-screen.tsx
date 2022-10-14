957f43a14421aa5168d6fb8570dad75936958b87
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-10-14 13:24:32 +0700

    working location notification, giving users their location when they go to the map screen

--- src/screens/map-screen.tsx
+++ src/screens/map-screen.tsx
@@ -1,6 +1,35 @@
-import React from 'react';
-import { Text } from 'native-base';
+import React, { useState, useEffect } from 'react';
+import { Box, Text } from 'native-base';
+import * as Location from 'expo-location';
 
 export const MapScreen = () => {
-    return <Text>Hello map</Text>;
+    const [location, setLocation] = useState<object | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
+    return (
+        <Box>
+            <Text>Hello map</Text>
+            <Text>{text}</Text>
+        </Box>
+    );
 };
