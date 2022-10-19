55b8987ca9c3b40db8802a1b6588ce20580db168
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-10-19 13:36:49 +0700

    added loading indecator and now map shows your current location

--- src/screens/map-view-test.tsx
+++ src/screens/map-view-test.tsx
@@ -1,11 +1,47 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import { LocationArray } from 'src/types/products';
+import { ActivityIndicator } from 'react-native';
 
 export const MapViewTest = () => {
+    const [location, setLocation] = useState<LocationArray>();
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let locationz = await Location.getCurrentPositionAsync({});
+
+            // console.log(location.coords.latitude);
+            const loxz: LocationArray = [locationz.coords.latitude, locationz.coords.longitude];
+
+            setLocation(loxz);
+        })();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            {location ? (
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: location[0],
+                        longitude: location[1],
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            ) : (
+                <ActivityIndicator color="#36d7b7" />
+            )}
         </View>
     );
 };
